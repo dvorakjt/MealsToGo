43ddbcabed44c049ac3d0b155cf997301538b3c2
dvorakjt <dvorakjt@gmail.com>
2021-07-21 00:21:28 -0400

    style activity indicator with styled-components

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,5 +1,7 @@
 import React, {useState, useContext} from 'react';
-import {Searchbar, ActivityIndicator} from 'react-native-paper';
+import {View} from 'react-native';
+import styled from 'styled-components/native';
+import {Searchbar, ActivityIndicator, Colors} from 'react-native-paper';
 
 import {RestaurantInfoCard} from '../components/restaurant-info-card.component';
 import {SafeArea} from '../../../components/utility/safe-area.component';
@@ -15,6 +17,16 @@ export const RestaurantsScreen = () => {
 
   const {isLoading, restaurants, error} = useContext(RestaurantsContext);
 
+  const Loading = styled(ActivityIndicator)`
+    margin-left: -25px;
+  `;
+
+  const LoadingContainer = styled.View`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+  `;
+
   return (
     <SafeArea>
       <RestaurantSearchView>
@@ -25,7 +37,9 @@ export const RestaurantsScreen = () => {
         />
       </RestaurantSearchView>
       {isLoading ? (
-        <ActivityIndicator />
+        <LoadingContainer>
+          <Loading animating={true} size={50} color={Colors.red800} />
+        </LoadingContainer>
       ) : (
         <RestaurantList
           data={restaurants}
